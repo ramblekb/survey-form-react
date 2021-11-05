e2c4c5be26fd144f801490e218ad9f9657b7bba4
ramblekb <ramblekb@gmail.com>
2021-11-05 04:20:10 -0500

    adding questions

--- client/src/api/quizQuestions.js
+++ client/src/api/quizQuestions.js
@@ -24,182 +24,182 @@ var quizQuestions = [
             }
         ]
     },
-    // {
-    //     question: "Feeling down, depressed, or hopeless?",
-    //     answers: [
-    //         {
-    //             type: "none",
-    //             content: "Not at all",
-    //             number: 0
-    //         },
-    //         {
-    //             type: "mild",
-    //             content: "Several days",
-    //             number: 1
-    //         },
-    //         {
-    //             type: "moderate",
-    //             content: "More than half the days",
-    //             number: 2
-    //         },
-    //         {
-    //             type: "moderately severe",
-    //             content: "Nearly every day",
-    //             number: 3
-    //         }
-    //     ]
-    // },
+    {
+        question: "Feeling down, depressed, or hopeless?",
+        answers: [
+            {
+                type: "none",
+                content: "Not at all",
+                number: 0
+            },
+            {
+                type: "mild",
+                content: "Several days",
+                number: 1
+            },
+            {
+                type: "moderate",
+                content: "More than half the days",
+                number: 2
+            },
+            {
+                type: "moderately severe",
+                content: "Nearly every day",
+                number: 3
+            }
+        ]
+    },
 
-    // {
-    //     question: "Trouble falling or staying asleep, or sleeping too much?",
-    //     answers: [
-    //         {
-    //             type: "none",
-    //             content: "Not at all",
-    //             number: 0
-    //         },
-    //         {
-    //             type: "mild",
-    //             content: "Several days",
-    //             number: 1
-    //         },
-    //         {
-    //             type: "moderate",
-    //             content: "More than half the days",
-    //             number: 2
-    //         },
-    //         {
-    //             type: "moderately severe",
-    //             content: "Nearly every day",
-    //             number: 3
-    //         }
-    //     ]
-    // },
-    // {
-    //     question: "Feeling tired or having little energy?",
-    //     answers: [
-    //         {
-    //             type: "none",
-    //             content: "Not at all",
-    //             number: 0
-    //         },
-    //         {
-    //             type: "mild",
-    //             content: "Several days",
-    //             number: 1
-    //         },
-    //         {
-    //             type: "moderate",
-    //             content: "More than half the days",
-    //             number: 2
-    //         },
-    //         {
-    //             type: "moderately severe",
-    //             content: "Nearly every day",
-    //             number: 3
-    //         }
-    //     ]
-    // },
-    // {
-    //     question: "Poor appetite or overeating?",
-    //     answers: [
-    //         {
-    //             type: "none",
-    //             content: "Not at all",
-    //             number: 0
-    //         },
-    //         {
-    //             type: "mild",
-    //             content: "Several days",
-    //             number: 1
-    //         },
-    //         {
-    //             type: "moderate",
-    //             content: "More than half the days",
-    //             number: 2
-    //         },
-    //         {
-    //             type: "moderately severe",
-    //             content: "Nearly every day",
-    //             number: 3
-    //         }
-    //     ]
-    // },
-    // {
-    //     question: "Poor appetite or overeating?",
-    //     answers: [
-    //         {
-    //             type: "none",
-    //             content: "Not at all",
-    //             number: 0
-    //         },
-    //         {
-    //             type: "mild",
-    //             content: "Several days",
-    //             number: 1
-    //         },
-    //         {
-    //             type: "moderate",
-    //             content: "More than half the days",
-    //             number: 2
-    //         },
-    //         {
-    //             type: "moderately severe",
-    //             content: "Nearly every day",
-    //             number: 3
-    //         }
-    //     ]
-    // },
-    // {
-    //     question: "Poor appetite or overeating?",
-    //     answers: [
-    //         {
-    //             type: "none",
-    //             content: "Not at all",
-    //             number: 0
-    //         },
-    //         {
-    //             type: "mild",
-    //             content: "Several days",
-    //             number: 1
-    //         },
-    //         {
-    //             type: "moderate",
-    //             content: "More than half the days",
-    //             number: 2
-    //         },
-    //         {
-    //             type: "moderately severe",
-    //             content: "Nearly every day",
-    //             number: 3
-    //         }
-    //     ]
-    // },
-    // {
-    //     question: "Poor appetite or overeating?",
-    //     answers: [
-    //         {
-    //             type: "none",
-    //             content: "Not at all",
-    //             number: 0
-    //         },
-    //         {
-    //             type: "mild",
-    //             content: "Several days",
-    //             number: 1
-    //         },
-    //         {
-    //             type: "moderate",
-    //             content: "More than half the days",
-    //             number: 2
-    //         },
-    //         {
-    //             type: "moderately severe",
-    //             content: "Nearly every day",
-    //             number: 3
-    //         }
-    //     ]
-    // },
+    {
+        question: "Trouble falling or staying asleep, or sleeping too much?",
+        answers: [
+            {
+                type: "none",
+                content: "Not at all",
+                number: 0
+            },
+            {
+                type: "mild",
+                content: "Several days",
+                number: 1
+            },
+            {
+                type: "moderate",
+                content: "More than half the days",
+                number: 2
+            },
+            {
+                type: "moderately severe",
+                content: "Nearly every day",
+                number: 3
+            }
+        ]
+    },
+    {
+        question: "Feeling tired or having little energy?",
+        answers: [
+            {
+                type: "none",
+                content: "Not at all",
+                number: 0
+            },
+            {
+                type: "mild",
+                content: "Several days",
+                number: 1
+            },
+            {
+                type: "moderate",
+                content: "More than half the days",
+                number: 2
+            },
+            {
+                type: "moderately severe",
+                content: "Nearly every day",
+                number: 3
+            }
+        ]
+    },
+    {
+        question: "Poor appetite or overeating?",
+        answers: [
+            {
+                type: "none",
+                content: "Not at all",
+                number: 0
+            },
+            {
+                type: "mild",
+                content: "Several days",
+                number: 1
+            },
+            {
+                type: "moderate",
+                content: "More than half the days",
+                number: 2
+            },
+            {
+                type: "moderately severe",
+                content: "Nearly every day",
+                number: 3
+            }
+        ]
+    },
+    {
+        question: "Poor appetite or overeating?",
+        answers: [
+            {
+                type: "none",
+                content: "Not at all",
+                number: 0
+            },
+            {
+                type: "mild",
+                content: "Several days",
+                number: 1
+            },
+            {
+                type: "moderate",
+                content: "More than half the days",
+                number: 2
+            },
+            {
+                type: "moderately severe",
+                content: "Nearly every day",
+                number: 3
+            }
+        ]
+    },
+    {
+        question: "Poor appetite or overeating?",
+        answers: [
+            {
+                type: "none",
+                content: "Not at all",
+                number: 0
+            },
+            {
+                type: "mild",
+                content: "Several days",
+                number: 1
+            },
+            {
+                type: "moderate",
+                content: "More than half the days",
+                number: 2
+            },
+            {
+                type: "moderately severe",
+                content: "Nearly every day",
+                number: 3
+            }
+        ]
+    },
+    {
+        question: "Poor appetite or overeating?",
+        answers: [
+            {
+                type: "none",
+                content: "Not at all",
+                number: 0
+            },
+            {
+                type: "mild",
+                content: "Several days",
+                number: 1
+            },
+            {
+                type: "moderate",
+                content: "More than half the days",
+                number: 2
+            },
+            {
+                type: "moderately severe",
+                content: "Nearly every day",
+                number: 3
+            }
+        ]
+    },
     {
         question: "Poor appetite or overeating?",
         answers: [
